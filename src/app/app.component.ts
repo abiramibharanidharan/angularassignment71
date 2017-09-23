f507ba4197a0e594a3c3eaa8176761bb4f4339b4
abiramibharanidharan <abiramibharanidharan@gmail.com>
2017-09-24 03:41:22 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'My-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  Name:string    //creating class variable
+  LastName:string
+  title:string
+  constructor(){} 
+  ngOnInit()
+  {
+    this.Name=""
+    this.LastName="" 
+  }    
+   displaytitle()
+   {  
+      this.title="Hello"
+   }  
 }
